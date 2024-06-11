4d0e9ea052b473bd53c5365237963755b405a079
exyyyl <79035965+Exyl22@users.noreply.github.com>
2024-06-12 01:00:20 +0300

    update: 0.951v

--- server/config.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-const mysql = require("mysql2");
-
-const urlDB = `mysql://root:nzqHCdnxzETDuwPMjqxJrXMxiVxuEfRj@viaduct.proxy.rlwy.net:17583/railway`
-const connection = mysql.createConnection();
-
-module.exports = connection;
